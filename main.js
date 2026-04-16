353524ea83d861c530b3d6f596ecf27cf59ad754
Izzatulloh Eshimboyev <eshimboevizzatullos@gmail.com>
2026-04-16 19:10:01 +0500

    site created. html and css added

---
+++ main.js
@@ -0,0 +1,245 @@
+// let son = +prompt("son kiriting");
+// let amal = prompt("+,-,*,/");
+// let son1 = +prompt("son kiriting");
+
+// let javob;
+// switch (amal) {
+//   case "+":
+//     natija = son + son1;
+
+//     break;
+
+//   case "-":
+//     natija = son - son1;
+
+//     break;
+
+//   case "*":
+//     natija = son * son1;
+
+//     break;
+
+//   case "/":
+//     natija = son / son1;
+// }
+
+// console.log(natija);
+
+// let num = +prompt("ikki xonali son kiriting");
+
+// let birlik = num % 10;
+
+// let onlik = Math.floor(num / 10) % 10;
+
+// switch (onlik) {
+//   case 1:
+//     console.log("o'n");
+
+//     break;
+
+//   case 2:
+//     console.log("yigirma");
+
+//     break;
+
+//   case 3:
+//     console.log("o'ttiz");
+
+//     break;
+//   case 4:
+//     console.log("qirq");
+
+//     break;
+//   case 5:
+//     console.log("'ellik'");
+
+//     break;
+//   case 6:
+//     console.log("oltmish");
+
+//     break;
+//   case 7:
+//     console.log("yetmish");
+
+//     break;
+//   case 8:
+//     console.log("sakson");
+
+//     break;
+//   case 9:
+//     console.log("to'qson");
+// }
+
+// switch (birlik) {
+//   case 1:
+//     console.log("bir");
+
+//     break;
+
+//   case 2:
+//     console.log("ikki");
+
+//     break;
+
+//   case 3:
+//     console.log("uch");
+
+//     break;
+//   case 4:
+//     console.log("to'rt");
+
+//     break;
+//   case 5:
+//     console.log("'besh'");
+
+//     break;
+//   case 6:
+//     console.log("olti");
+
+//     break;
+//   case 7:
+//     console.log("yetti");
+
+//     break;
+//   case 8:
+//     console.log("sakkiz");
+
+//     break;
+//   case 9:
+//     console.log("to'qqiz");
+// }
+
+//===========================================================================================
+
+//3-masala
+
+// let num = +prompt("uch xonali son kiriting");
+
+// let birlik = num % 10;
+
+// let onlik = Math.floor(num / 10) % 10;
+
+// let yuzlik = Math.floor(num / 100) % 10;
+
+// let str = " ";
+
+// switch (yuzlik) {
+//   case 1:
+//     str += "biryuz ";
+
+//     break;
+
+//   case 2:
+//     str += "ikkiyuz ";
+
+//     break;
+
+//   case 3:
+//     str += "uchyuz ";
+
+//     break;
+//   case 4:
+//     str += "to'rtyuz ";
+
+//     break;
+//   case 5:
+//     str += "beshyuz ";
+
+//     break;
+//   case 6:
+//     str += "oltiyuz ";
+
+//     break;
+//   case 7:
+//     str += "yettiyuz ";
+
+//     break;
+//   case 8:
+//     str += "sakkizyuz ";
+
+//     break;
+//   case 9:
+//     str += "to'qqizyuz ";
+// }
+
+// switch (onlik) {
+//   case 1:
+//     str += "o'n ";
+
+//     break;
+
+//   case 2:
+//     str += "yigirma ";
+
+//     break;
+
+//   case 3:
+//     str += "o'ttiz ";
+
+//     break;
+//   case 4:
+//     str += "qirq ";
+
+//     break;
+//   case 5:
+//     str += "ellik ";
+
+//     break;
+//   case 6:
+//     str += "oltmish ";
+
+//     break;
+//   case 7:
+//     str += "yetmish ";
+
+//     break;
+//   case 8:
+//     str += "sakson ";
+
+//     break;
+//   case 9:
+//     str += "to'qson ";
+// }
+
+// switch (birlik) {
+//   case 1:
+//     str += "bir ";
+
+//     break;
+
+//   case 2:
+//     str += "ikki ";
+
+//     break;
+
+//   case 3:
+//     str += "uch ";
+
+//     break;
+//   case 4:
+//     str += "to'rt ";
+
+//     break;
+//   case 5:
+//     str += "besh ";
+
+//     break;
+//   case 6:
+//     str += "olti ";
+
+//     break;
+//   case 7:
+//     str += "yetti ";
+
+//     break;
+//   case 8:
+//     str += "sakkiz ";
+
+//     break;
+//   case 9:
+//     str += "to'qqiz ";
+// }
+
+// console.log(str);
+
+
