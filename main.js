553b45010c6fea8b41cd6f30d42df00c21a4f107
Izzatulloh Eshimboyev <eshimboevizzatullos@gmail.com>
2026-04-17 14:06:34 +0500

    4 issues worked

--- main.js
+++ main.js
@@ -1,245 +1,70 @@
-// let son = +prompt("son kiriting");
-// let amal = prompt("+,-,*,/");
-// let son1 = +prompt("son kiriting");
-
-// let javob;
-// switch (amal) {
-//   case "+":
-//     natija = son + son1;
-
-//     break;
-
-//   case "-":
-//     natija = son - son1;
-
-//     break;
-
-//   case "*":
-//     natija = son * son1;
-
-//     break;
-
-//   case "/":
-//     natija = son / son1;
-// }
-
-// console.log(natija);
-
-// let num = +prompt("ikki xonali son kiriting");
-
-// let birlik = num % 10;
-
-// let onlik = Math.floor(num / 10) % 10;
-
-// switch (onlik) {
+// let ball = +prompt("balingizni kiriting");
+// switch (ball) {
 //   case 1:
-//     console.log("o'n");
+//     console.log("yomon");
 
 //     break;
-
 //   case 2:
-//     console.log("yigirma");
+//     console.log("qoniqarsiz");
 
 //     break;
-
 //   case 3:
-//     console.log("o'ttiz");
+//     console.log("qoniqarli");
 
 //     break;
 //   case 4:
-//     console.log("qirq");
+//     console.log("yaxshi");
 
 //     break;
 //   case 5:
-//     console.log("'ellik'");
+//     console.log("a'lo");
 
 //     break;
-//   case 6:
-//     console.log("oltmish");
-
-//     break;
-//   case 7:
-//     console.log("yetmish");
-
-//     break;
-//   case 8:
-//     console.log("sakson");
-
-//     break;
-//   case 9:
-//     console.log("to'qson");
+//   default:
+//     console.log("bunday ball yo'q");
 // }
 
-// switch (birlik) {
+//==================================================================================
+
+//2-masala
+
+// let oy = +prompt("oyni raqamini kiriting");
+// switch (oy) {
+//   case 12:
 //   case 1:
-//     console.log("bir");
-
-//     break;
-
 //   case 2:
-//     console.log("ikki");
-
+//     console.log("qish");
 //     break;
-
 //   case 3:
-//     console.log("uch");
-
-//     break;
 //   case 4:
-//     console.log("to'rt");
-
-//     break;
 //   case 5:
-//     console.log("'besh'");
-
+//     console.log("bahor");
 //     break;
 //   case 6:
-//     console.log("olti");
-
-//     break;
 //   case 7:
-//     console.log("yetti");
-
-//     break;
 //   case 8:
-//     console.log("sakkiz");
-
+//     console.log("yoz");
 //     break;
 //   case 9:
-//     console.log("to'qqiz");
+//   case 10:
+//   case 11:
+//     console.log("kuz");
+//     break;
+//   default:
+//     console.log("bunday oy mavjud emas");
 // }
 
-//===========================================================================================
+//============================================================================
 
 //3-masala
 
-// let num = +prompt("uch xonali son kiriting");
-
-// let birlik = num % 10;
-
-// let onlik = Math.floor(num / 10) % 10;
-
-// let yuzlik = Math.floor(num / 100) % 10;
-
-// let str = " ";
-
-// switch (yuzlik) {
-//   case 1:
-//     str += "biryuz ";
-
-//     break;
-
-//   case 2:
-//     str += "ikkiyuz ";
-
-//     break;
-
-//   case 3:
-//     str += "uchyuz ";
-
-//     break;
-//   case 4:
-//     str += "to'rtyuz ";
-
-//     break;
-//   case 5:
-//     str += "beshyuz ";
-
-//     break;
-//   case 6:
-//     str += "oltiyuz ";
-
-//     break;
-//   case 7:
-//     str += "yettiyuz ";
-
-//     break;
-//   case 8:
-//     str += "sakkizyuz ";
-
-//     break;
-//   case 9:
-//     str += "to'qqizyuz ";
+// let yil = +prompt("yil kiriting");
+// if ((yil % 100 !== 0 && yil % 4 === 0) || yil % 400 === 0) {
+//   console.log("kabisa yili");
+// } else {
+//   console.log("oddiy yil");
 // }
 
-// switch (onlik) {
-//   case 1:
-//     str += "o'n ";
+//=========================================================================================
 
-//     break;
-
-//   case 2:
-//     str += "yigirma ";
-
-//     break;
-
-//   case 3:
-//     str += "o'ttiz ";
-
-//     break;
-//   case 4:
-//     str += "qirq ";
-
-//     break;
-//   case 5:
-//     str += "ellik ";
-
-//     break;
-//   case 6:
-//     str += "oltmish ";
-
-//     break;
-//   case 7:
-//     str += "yetmish ";
-
-//     break;
-//   case 8:
-//     str += "sakson ";
-
-//     break;
-//   case 9:
-//     str += "to'qson ";
-// }
-
-// switch (birlik) {
-//   case 1:
-//     str += "bir ";
-
-//     break;
-
-//   case 2:
-//     str += "ikki ";
-
-//     break;
-
-//   case 3:
-//     str += "uch ";
-
-//     break;
-//   case 4:
-//     str += "to'rt ";
-
-//     break;
-//   case 5:
-//     str += "besh ";
-
-//     break;
-//   case 6:
-//     str += "olti ";
-
-//     break;
-//   case 7:
-//     str += "yetti ";
-
-//     break;
-//   case 8:
-//     str += "sakkiz ";
-
-//     break;
-//   case 9:
-//     str += "to'qqiz ";
-// }
-
-// console.log(str);
-
-
+//4-masala
